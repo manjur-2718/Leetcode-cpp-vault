31f6fd16e3cc3e9b94201aff1b3324f675755ca7
Manjur Hossain <manjurhossain2006@gmail.com>
2026-08-31 23:37:54 +0530

    Time: 11 ms (39.54%), Space: 44.7 MB (78.23%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int j=0;
+        int i=0;
+        while(j<g.size() && i<s.size()){
+            if(g[j]<=s[i]){   
+                j++;
+            }
+            i++;
+        }
+        return j;
+    }
+};
